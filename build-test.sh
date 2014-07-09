2208a176fa19cd3faca23d6757e787bbe46df67f
bencomp <ben@companjen.name>
2014-07-09 17:56:09 +0200

    Add XML Schema validation of transformed XML files

--- build-test.sh
+++ build-test.sh
@@ -5,3 +5,4 @@ mkdir eadtest
 cd eadtest
 java -Xmx1g -jar ~/Applications/saxon9he.jar -s:/Users/ben/Documents/Projecten/EHRI/ushmm/3000docs.xml -xsl:../solr2ead.xsl
 cd ..
+xmllint --noout --schema ead.xsd eadtest/ead/*.xml
