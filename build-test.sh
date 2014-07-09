556d9ba232621e4ba4111bb15233e527e1c1c55d
bencomp <ben@companjen.name>
2014-07-09 16:45:18 +0200

    Put build script in repo

---
+++ build-test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+[ -e eadtest ] && rm -r eadtest
+mkdir eadtest
+cd eadtest
+java -Xmx1g -jar ~/Applications/saxon9he.jar -s:/Users/ben/Documents/Projecten/EHRI/ushmm/3000docs.xml -xsl:solr2ead.xsl
+cd ..
