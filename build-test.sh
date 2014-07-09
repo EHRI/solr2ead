8d9235ccf79d1383a9014c3a677eed2b06b87851
bencomp <ben@companjen.name>
2014-07-09 16:57:11 +0200

    Use correct relative path for XSLT, git-ignore the output directory

--- build-test.sh
+++ build-test.sh
@@ -3,5 +3,5 @@
 [ -e eadtest ] && rm -r eadtest
 mkdir eadtest
 cd eadtest
-java -Xmx1g -jar ~/Applications/saxon9he.jar -s:/Users/ben/Documents/Projecten/EHRI/ushmm/3000docs.xml -xsl:solr2ead.xsl
+java -Xmx1g -jar ~/Applications/saxon9he.jar -s:/Users/ben/Documents/Projecten/EHRI/ushmm/3000docs.xml -xsl:../solr2ead.xsl
 cd ..
