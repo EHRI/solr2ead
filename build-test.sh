2f5161649f6e5dcf210ae33e9545b7a5cc1545aa
bencomp <ben@companjen.name>
2014-07-31 13:55:07 +0200

    Add XML validation of EAD files  to build script

--- build-test.sh
+++ build-test.sh
@@ -3,6 +3,6 @@
 [ -e eadtest ] && rm -r eadtest
 mkdir eadtest
 cd eadtest
-java -Xmx1g -jar ~/Applications/saxon9he.jar -s:/Users/ben/Documents/Projecten/EHRI/ushmm/3000docs.xml -xsl:../solr2ead.xsl
+java -Xmx1g -jar ~/Applications/saxon9he.jar -s:/Users/ben/Documents/Projecten/EHRI/ushmm/1000docs2.xml -xsl:../solr2ead.xsl
 cd ..
-xmllint --noout --schema ead.xsd eadtest/ead/*.xml 2> ead-validation-results.txt
+xmllint --noout --schema ead.xsd eadtest/ead/* 2> validation.log
