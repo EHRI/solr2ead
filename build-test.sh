d6889f610cd4505922632d681d04cdcdda6107ad
bencomp <ben@companjen.name>
2014-09-08 16:45:27 +0200

    Put the EAD files in directories by record type

--- build-test.sh
+++ build-test.sh
@@ -5,4 +5,6 @@ mkdir eadtest
 cd eadtest
 java -Xmx1g -jar ~/Applications/saxon9he.jar -explain -traceout:trace.txt -s:/Users/ben/Documents/Projecten/EHRI/ushmm/1000docs2.xml -xsl:../solr2ead.xsl
 cd ..
-xmllint --noout --schema ead.xsd eadtest/ead/* 2> validation.log
+for FILE in `ls eadtest/ead/`; do
+    xmllint --noout --schema ead.xsd eadtest/ead/$FILE/* 2>> validation.log
+done
