c073ab44522566af6d3f519a713173f438748ee8
bencomp <ben@companjen.name>
2014-09-09 15:35:41 +0200

    Use correct path for XML Schema in validation script

--- validate-all.sh
+++ validate-all.sh
@@ -2,6 +2,6 @@
 
 cd eadtest/ead
 for FILE in `ls ./`; do
-    xmllint --noout --schema ead.xsd $FILE/* 2> validation-$FILE.log
+    xmllint --noout --schema ../../ead.xsd $FILE/* 2> validation-$FILE.log
 done
 cd ../..
