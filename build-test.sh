a79cec2aacbeba180abc700a1c645039a27c4fae
Mike Bryant <mikesname@gmail.com>
2017-03-28 10:57:44 +0100

    Update to reflect use of Solr query, rather than update, XML.
    Additional tweaks possibly needed to better handle multivalue fields.

--- build-test.sh
+++ build-test.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+FILE=$1
+
 [ -e eadtest ] && rm -r eadtest
 mkdir eadtest
 cd eadtest
-java -Xmx1g -jar ~/Applications/saxon9he.jar -explain -traceout:trace.txt -s:/Users/ben/Documents/Projecten/EHRI/ushmm/1000docs2.xml -xsl:../solr2ead.xsl
+java -Xmx1g -jar ../saxon9he.jar -explain -traceout:trace.txt -s:../$FILE -xsl:../solr2ead.xsl
 cd ..
 for FILE in `ls eadtest/ead/`; do
     xmllint --noout --schema ead.xsd eadtest/ead/$FILE/* 2>> validation.log
